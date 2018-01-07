e7d251e4193f07906bc95d23a2c4dbc3da2472b1
jacklee <jacklee@jack.localhost>
2018-01-07 16:55:06 +0800

    modify sklearn2.py

--- py2/sklearn2.py
+++ py2/sklearn2.py
@@ -26,17 +26,12 @@ matrices. Other types that are convertible to numeric arrays, such as Pandas
 DataFrame,are also acceptable.
 """
 X = np.random.random((10,5))
-#print(X)
 y = np.array(['M','M','F','F','M','F','M','M','F','F'])
-print(y)
 X[X < 0.7] = 0.5
-print(X)
 
 
 # Training And Test Data
 X_train,X_test,y_train,y_test = train_test_split(X, y, random_state=0)
-print(y_test)
-print(y_train)
 
 
 # Preprocessing The Data
@@ -45,20 +40,15 @@ from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler().fit(X_train)
 standardized_X = scaler.transform(X_train)
 standardized_X_test = scaler.transform(X_test)
-#print(standardized_X)
-#print(standardized_X_test)
 # Normalization
 from sklearn.preprocessing import Normalizer
 scaler = Normalizer().fit(X_train)
 normalized_X = scaler.transform(X_train)
 normalized_X_test = scaler.transform(X_test)
-print(normalized_X)
-print(normalized_X_test)
 # Binarization
 from sklearn.preprocessing import Binarizer
 binarizer = Binarizer(threshold=0.0).fit(X)
 binary_X = binarizer.transform(X)
-#print(binary_X)
 # Create Your Model
 # Supervised Learning Estimators
 # Linear Regression
@@ -68,7 +58,7 @@ lr = LinearRegression(normalize=True)
 from sklearn.svm import SVC
 svc = SVC(kernel='linear')
 # Naive Bayes
-from sklearn.naive_bayes
+from sklearn.naive_bayes import GaussianNB
 gnb = GaussianNB()
 # KNN
 from sklearn import neighbors
@@ -83,7 +73,8 @@ from sklearn.cluster import KMeans
 k_means = KMeans(n_clusters=3, random_state=0)
 
 # Model Fitting
-Supervised learning
+#Supervised learning
+X, y = iris.data[:,:2], iris.target
 lr.fit(X,y)
 knn.fit(X_train, y_train)
 svc.fit(X_train, y_train)
@@ -92,6 +83,28 @@ svc.fit(X_train, y_train)
 k_means.fit(X_train)                          # Fit the model to the data
 pca_model = pca.fit_transform(X_train)        # Fit to data, then transform it
 
+# Prediction
+# Supervised Estimators
+y_pred = svc.predict(np.random.random((2,5)))  # Predict labels
+y_pred = lr.predict(X_test)                   # Predict labels
+y_pred = knn.predict_proba(X_test)            # Estimate probabiity of a label
+# Unsupervised Estimators
+y_pred = k_means.predict(X_test)              # Predict labels in clustering algos
+
+# Encoding Categorical Features
+from sklearn.preprocessing import LabelEncoder
+enc = LabelEncoder()
+y = enc.fit_transform(y)
+
+# Imputing Missing Values
+from sklearn.preprocessing import Imputer
+imp = Imputer(missing_values=0, strategy='mean',axis=0)
+imp.fit_transform(X_train)
+
+# Generating Polynormial Features
+from sklearn.preprocessing import PolynomialFeatures
+poly = PolynomialFeatures(5)
+poly.fit_transform(X)
 # Evaluate Your Model's Performance
 # Classification Metrics
 # Accuracy Score
@@ -138,4 +151,23 @@ print(cross_val_score(lr, X, y,cv=2))
 
 # Tune Your Model
 # Grid Search
+from sklearn.grid_search import GridSearchCV
+params = {"n_neighbors":np.arange(1,3),
+          "metrics":["euclidean","cityblock"]}
+grid = GridSearchCV(estimator=knn,
+                    param_grid=params)
+grid.fit(X_train, y_train)
+print(grid.best_score_)
+print(grid.best_estimator_.n_neightors)
 
+# Randomized Parameter Optimization
+from sklearn.grid_search import RandomizedSearchCV
+params = {"n_neighbors":range(1,5),
+          "weights":["uniform","distance"]}
+rsearch = RandomizedSearhCV(estimator=knn,
+                            param_distributions=params,
+                            cv=4,
+                            n_iter=8,
+                            radom_state=5)
+rsearch.fit(X_train, y_train)
+print(rsearch.best_score_)
